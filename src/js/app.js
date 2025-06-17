e001d73ecf435a0d8b714435181f9a7d7dc3f26d
Chambonini <137449949+Chambonini@users.noreply.github.com>
2025-06-17 00:07:43 +0000

    confirmo cambios

--- src/js/app.js
+++ src/js/app.js
@@ -26,21 +26,48 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  let cover = `
+    <div class="cover">
+      <img src="${variables.background}" />
+    </div>
+`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "First Name" : variables.name} 
+          ${variables.lastName == null ? "Last Name" : variables.lastName}</h1> 
+         <h2>${variables.role == null ? " Your Role" : variables.role} </h2>
+          <h3>${variables.city == null ? " Your City" : variables.city} 
+          ${
+            variables.country == null ? "/ Your country" : variables.country
+          }</h3>
+         
+            <ul class="position-right">
+            <li><a href="${
+              variables.twitter
+                ? ` https://twitter.com/${variables.twitter}`
+                : ` https://twitter.com/4geeksacademy`
+            }">
+            <i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github
+                ? ` https://github.com/${variables.github}`
+                : ` https://github.com/4geeksacademy`
+            }">
+            <i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+                ? ` https://linkedin.com/in/${variables.linkedin}`
+                : ` https://linkedin.com/school/4geeksacademy`
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+                ? ` https://instagram.com/${variables.instagram}`
+                : ` https://instagram.com/4geeksacademy`
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
